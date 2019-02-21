314e28295ad07add58da5e9143913c7ae58d066d
William Fleming <chaosmegaman@Williams-MacBook-Pro.local>
2019-02-21 16:07:01 -0500

    finished weather controller

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -7,6 +7,35 @@ const weatherController = {
             res.send(weathers)
         })
     },
+
+    show: (req, res) => {
+        Weather.findById(req.params.weatheId)
+        .then((weather) =>{
+            res.send(weather)
+        })
+    },
+
+    create: (req, res) => {
+        Weather.create(req.body)
+        .then((weather)=>{
+            res.send(weather)
+        })
+    },
+
+    patch: (req, res) =>{
+        Weather.findByIdAndUpdate(req.params.weatherId, req.body)
+        .then((updatedWeather) => {
+            updatedWeather.save()
+            res.send(updatedWeather)
+        })
+    },
+
+    delete: (req, res) => {
+        Weather.findByIdAndDelete(req.params.weatherId)
+        .then(()=>{
+            res.send(200)
+        })
+    }
 }
 
 module.exports = weatherController
