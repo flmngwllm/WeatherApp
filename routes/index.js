28ae7b629afb095e3449d3ec7d864f5aaeb685b1
William Fleming <chaosmegaman@Williams-MacBook-Pro.local>
2019-02-21 15:08:43 -0500

    added routes to index.js

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const weatherController = require('../controllers/weatherController')
+
+router.get('api/weather', weatherController.index),
+router.post('api/weather', weatherController.create)
+router.get('api/weather/:weatherId', weatherController.show),
+router.patch('api/weather/:weatherId', weatherController.patch),
+router.delete('api/weather/:weatherId', weatherController.delete)
+
+module.exports = router
