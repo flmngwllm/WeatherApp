4cf9a0ed268b77b1434ad988b962a2074a00fb2d
William Fleming <chaosmegaman@Williams-MacBook-Pro.local>
2019-02-21 14:59:39 -0500

    added to my server file

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 const express =  require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
+const logger = require('morgan')
+const routes = require('./routes/index')
 const app = express()
 
+app.use(logger('dev'))
 app.use(express.urlencoded({
     extended: true
 }))
 
 app.use(express.json())
 
+app.use('/', routes)
+
 app.use(express.static(`${__dirname}/client/build`))
+
+app.get('/*', (req, res)=>{
+    res.sendFile(`${__dirname}/client/build/index.html`)
+})
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT,()=>{
