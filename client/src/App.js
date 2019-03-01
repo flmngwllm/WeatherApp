642da361487e0275d4b76d0c29b1e76b93af458d
William Fleming <chaosmegaman@Williams-MacBook-Pro.local>
2019-03-01 18:48:55 -0500

    added route

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <p>Hello</p>
+        <Router>
+          <Switch>
+            <Route exact path="/weather" component={} />
+          </Switch>
+        </Router>
       </div>
     );
   }
