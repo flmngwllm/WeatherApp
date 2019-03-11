55894664a85a47c3890c183e36c3f69f8383fbef
William Fleming <chaosmegaman@Williams-MacBook-Pro.local>
2019-03-11 13:03:43 -0400

    setting up api

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,15 +1,18 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import styled from 'styled-components';
+
+
 
 
 class NavBar extends Component {
     render(){
         return(
             <div>
-                <Link to="/Weather">Weather</Link>
+                <Link to="/weather">Weather</Link>
                 <Link to="/">Home</Link>
             </div>
-        )
+        );
     }
 }
 
